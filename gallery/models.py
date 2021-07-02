e29148f1ef180872ba1f6f0b172d1f9fa760809e
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-07-03 00:11:55 +0300

    create model images

--- gallery/models.py
+++ gallery/models.py
@@ -1,24 +1,29 @@
 from django.db import models
-from django.db.models.fields import CharField, EmailField
 
 # Create your models here.
 class Photographer(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    email = EmailField()
+    email = models.EmailField()
     
     def __str__(self):
         return self.first_name
 
 class Category(models.Model):
-    category_name = CharField(max_length=30)
+    category_name = models.CharField(max_length=30)
 
     def __str__(self):
         return self.category_name
 
 class Location(models.Model):
-    location_name = CharField(max_length=30)
+    location_name = models.CharField(max_length=30)
 
     def __str__(self):
         return self.location_name
 
+class Images(models.Model):
+    image_name = models.CharField(max_length=30)
+    image_description = models.TextField()
+    image = models.ImageField(upload_to='galleries/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)

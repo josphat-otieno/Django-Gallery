bda2baa52fe1a72b50a06738525874f24e42d384
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-07-02 23:57:27 +0300

    create model photograher

--- gallery/models.py
+++ gallery/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django.db.models.fields import CharField, EmailField
 
 # Create your models here.
+class Photographer(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = EmailField()
+    
+    def __str__(self):
+        return self.first_name
 

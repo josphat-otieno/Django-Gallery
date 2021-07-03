c6f1d28c7f007c4354cc77faf4512d749a93703a
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-07-03 15:17:59 +0300

    update views

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .models import Category, Location, Images
 
 # Create your views here.
-def welcome(request):
-    return render(request, 'all-gallery/index.html')
+def index(request):
+
+    images = Images.objects.all()
+    categories = Category.objects.all()
+    locations = Location.objects.all()
+    context= {"images": images, "categories":categories, "locations":locations}
+    return render(request, 'all-gallery/index.html', context)
--- mygallery/urls.py
+++ mygallery/urls.py
@@ -19,6 +19,6 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^gallery/', include('gallery.urls'))
+    url(r'', include('gallery.urls'))
 ]
  

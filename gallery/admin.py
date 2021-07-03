e338ef235946bee0f8f3defa015294422aa4fd17
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-07-03 12:49:10 +0300

    register models

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Photographer, Category,Location,Images
 
 # Register your models here.
+admin.site.register(Photographer)
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(Images)
+
